7c18e7aa0634fc3309ca9f54e29f6f294a69926a
Aylinbaykan <aylinbaykan57@gmail.com>
2022-11-18 14:59:07 +0300

    initial commit

--- build/aylin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aylinbaykan/catkin_ws/src/aylin
-# Build directory: /home/aylinbaykan/catkin_ws/build/aylin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
